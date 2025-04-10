2f67e97cf413f638af402130ad67e554ca4a7e6b
Antonio <Antonio.Almena@missingcorner.com>
2025-04-10 14:49:09 -0700

    [9ntonio][Catch all page with article lookup service]

--- src/app/generic-page/generic-page.component.ts
+++ src/app/generic-page/generic-page.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule, Router } from '@angular/router';
+import { Subscription, Observable } from 'rxjs';
+import {
+	ContentfulService,
+	TypeArticlePageWithoutLinkResolutionResponse,
+} from 'public-library';
 
 @Component({
 	selector: 'app-generic-page',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [RouterModule, CommonModule],
 	templateUrl: './generic-page.component.html',
 	styleUrl: './generic-page.component.scss',
 })
-export class GenericPageComponent {}
+export class GenericPageComponent implements OnInit {
+	private readonly subscriptions = new Subscription();
+	public articleData$!: Observable<
+		TypeArticlePageWithoutLinkResolutionResponse[]
+	>;
 
+	constructor(
+		private readonly contentfulService: ContentfulService,
+		private readonly route: ActivatedRoute,
+		private readonly router: Router
+	) {}
+
+	ngOnInit() {
+		this.subscriptions.add(
+			this.route.url.subscribe((segments) => {
+				const slug = segments.map((segment) => segment.path).join('/');
+				if (slug) {
+					this.initArticle(slug);
+				} else {
+					console.log(
+						'%c No slug found ',
+						'color: white; background-color: #ff0000; padding: 2px; border-radius: 4px;'
+					);
+					// this.router.navigate(['/']);
+				}
+			})
+		);
+	}
+
+	private initArticle(slug: string) {
+		console.log(
+			`%c Init Article ${slug}`,
+			'color: white; background-color: #ff0000; padding: 2px; border-radius: 4px;'
+		);
+		this.articleData$ = this.contentfulService.getArticleEntries(slug);
+		this.articleData$.subscribe((data) => {
+			console.log(
+				`%c data length: ${data.length} `,
+				'color: white; background-color: #ff0000; padding: 2px; border-radius: 4px;'
+			);
+		});
+	}
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,37 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ChainModifiers } from 'contentful';
-import { Observable } from 'rxjs';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { Subscription } from 'rxjs';
 import {
 	ContentfulService,
-	TypeArticlePage,
 	StateService,
 } from 'public-library';
 
 @Component({
 	selector: 'app-home',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, RouterModule],
 	templateUrl: './home.component.html',
 	styleUrl: './home.component.scss',
 })
-
-export class HomeComponent implements OnInit {
-	public _contentfulData: TypeArticlePage<ChainModifiers> | null = null;
+export class HomeComponent implements OnInit, OnDestroy {
+	private readonly subscriptions = new Subscription();
 
 	constructor(
 		private readonly _contentfulService: ContentfulService,
-		private readonly _stateService: StateService
+		private readonly _stateService: StateService,
+		private readonly route: ActivatedRoute,
+		private readonly router: Router
 	) {}
 
 	ngOnInit() {
-		this._contentfulService.getEntries();
+		this.subscriptions.add(
+			this.route.url.subscribe((segments) => {
+				const slug = segments.map((segment) => segment.path).join('/');
+				const finSlug = slug === '' ? 'home' : slug;
+				console.log(
+					`%c URL slug: ${finSlug} `,
+					'color: white; background-color: #ff0000; padding: 2px; border-radius: 4px;'
+				);
+			})
+		);
 
-		this._contentfulService
-			.getPage()
-			.then((data: TypeArticlePage<ChainModifiers>) => {
-				this._contentfulData = data;
-				this._stateService.setContentfulData(this._contentfulData);
-			});
+		this._contentfulService.getEntry();
+	}
+
+	ngOnDestroy() {
+		this.subscriptions.unsubscribe();
 	}
 }

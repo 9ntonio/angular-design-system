2bcded2c560b9186c689c2d176a8607ec9efc9e5
Antonio <Antonio.Almena@missingcorner.com>
2025-04-14 18:02:01 -0700

    [9ntonio][Accordion Variations]

--- projects/public-library/src/lib/accordion/accordion.component.ts
+++ projects/public-library/src/lib/accordion/accordion.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from "@angular/common"
 import { BadgesComponent } from '../badges/badges.component';
 
@@ -9,17 +9,34 @@ import { BadgesComponent } from '../badges/badges.component';
   templateUrl: './accordion.component.html',
   styleUrl: './accordion.component.css'
 })
-export class AccordionComponent {
+export class AccordionComponent implements OnInit {
 	@Input() title!: string;
+	@Input() placement: 'first' | 'middle' | 'last' = 'first';
+
+	public _borderCss: string = '';
 
 	toggleDetails(event: MouseEvent, detailsElement: HTMLDetailsElement): void {
-		console.log('toggleDetails', event, detailsElement);
-		// Optional: We could prevent default and manually toggle for more control
+		console.log('---> toggleDetails');
+		// !! Optional: We could prevent default and manually toggle for more control
 		// event.preventDefault();
 		// detailsElement.open = !detailsElement.open;
-
-		// For more advanced animation, we would add code here
-		// to control height transition, etc.
 	  }
+
+	toggleObjDetails(event: MouseEvent, detailsObjElement: HTMLDetailsElement): void {
+		console.log('---> toggleObjDetails');
+		// !! Optional: We could prevent default and manually toggle for more control
+		// event.preventDefault();
+		// detailsElement.open = !detailsElement.open;
+	}
+
+	ngOnInit(): void {
+		if(this.placement === 'first') {
+			this._borderCss = 'rounded-t-xl';
+		} else if(this.placement === 'middle') {
+			this._borderCss = '';
+		} else if(this.placement === 'last') {
+			this._borderCss = 'rounded-b-xl';
+		}
+	}
 }
 
